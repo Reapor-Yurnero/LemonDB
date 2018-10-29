7d45d05410ab1c38394de596a407679450098ba5
dominicsjc <dominic@sjtu.edu.cn>
2018-10-29 18:07:43 +0800

    implementations of COUNT
    Former-commit-id: e92118e53b7a83028805be722d625df09dd720c7

--- src/query/QueryBuilders.cpp
+++ src/query/QueryBuilders.cpp
@@ -16,6 +16,7 @@
 #include "data/UpdateQuery.h"
 #include "data/SelectQuery.h"
 #include "data/DeleteQuery.h"
+#include "data/CountQuery.h"
 
 #include <iomanip>
 #include <iostream>
@@ -165,9 +166,9 @@ Query::Ptr ComplexQueryBuilder::tryExtractQuery(TokenizedQueryString &query) {
         /*return std::make_unique<DuplicateQuery>(
                 this->targetTable, this->operandToken, this->conditionToken);*/
     if (operation == "COUNT")
-        return std::make_unique<NopQuery>(); // Not implemented
-        /*return std::make_unique<CountQuery>(
-                this->targetTable, this->operandToken, this->conditionToken);*/
+        //return std::make_unique<NopQuery>(); // Not implemented
+        return std::make_unique<CountQuery>(
+                this->targetTable, this->operandToken, this->conditionToken);
     if (operation == "SUM")
         return std::make_unique<NopQuery>(); // Not implemented
         /*return std::make_unique<SumQuery>(
