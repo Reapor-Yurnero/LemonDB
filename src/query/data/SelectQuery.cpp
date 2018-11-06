142136222b54d72f28395f6373202180b1cf69ce
dominicsjc <dominic@sjtu.edu.cn>
2018-11-07 04:41:31 +0800

    Improvement of SELECT
    Former-commit-id: 1a78a63f062b98073d510bc6a082d5af789ead47

--- src/query/data/SelectQuery.cpp
+++ src/query/data/SelectQuery.cpp
@@ -19,27 +19,27 @@ QueryResult::Ptr SelectQuery::execute() {
                 "No operand (? operands)."_f % operands.size()
         );
     Database &db = Database::getInstance();
-    try{
+    try {
         auto &table = db[this->targetTable];
         if (this->operands[0] != "KEY")
             return make_unique<ErrorMsgResult>(
                     qname, "The beginning field is not KEY"
             );
         auto result = initCondition(table);
-        map<Table::KeyType, vector<Table::ValueType > > rowData;
+        map<Table::KeyType, vector<Table::ValueType *> > rowData;
         if (result.second) {
             for (auto table_it = table.begin(); table_it != table.end(); ++table_it) {
                 if (this->evalCondition(*table_it)) {
                     // todo: optimize tmp vector
-                    vector<Table::ValueType> datum;
+                    vector<Table::ValueType *> datum;
                     for (auto field_it = ++this->operands.begin(); field_it != this->operands.end(); ++field_it) {
-                        datum.emplace_back((*table_it)[*field_it]);
+                        datum.emplace_back(&((*table_it)[*field_it]));
                     }
-                    rowData.insert(make_pair(table_it->key(), datum));
+                    rowData.emplace(make_pair(table_it->key(), move(datum)));
                 }
             }
         }
-        return std::make_unique<AnswerMsgResult>(rowData);
+        return std::make_unique<AnswerMsgResult>(move(rowData));
     } catch (const TableNameNotFound &e) {
         return make_unique<ErrorMsgResult>(qname, this->targetTable, "No such table."s);
     } catch (const IllFormedQueryCondition &e) {
