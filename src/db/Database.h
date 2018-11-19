c70ba70ced9aee19b72598c6a527dca941f9075f
Reapor-Yurnero <reapor.yurnero@gmail.com>
2018-11-19 15:38:34 +0800

    add queryresults mutex
    Former-commit-id: f380e7ef7edee8b02653190045395ce671216b46

--- src/db/Database.h
+++ src/db/Database.h
@@ -47,6 +47,11 @@ private:
      */
     std::mutex table_lock_mutex;
 
+    /**
+     * The lock for queryresults
+     */
+    std::mutex queryresults_mutex;
+
 public:
     std::unordered_map<int,Query::Ptr> queries;
 
@@ -59,6 +64,7 @@ public:
     }
 
     void addresult(int id,QueryResult::Ptr &&queryresult){
+        std::unique_lock<std::mutex> lock(this->queryresults_mutex);
         this->queryresults.emplace(id,std::move(queryresult));
     }
 
